592b034cb134ac15f23acace0757fb7b4a9a92c3
Vinicius Cotrim <vinicius.cotrim.28@gmail.com>
2025-10-30 21:07:00 +0000

    Tarefa CRUD-1 Concluída: Implementação de Models, Views, URLs para Consulta e Filtro.

--- MeuProjeto/MeuSite/MeuSite/views.py
+++ MeuProjeto/MeuSite/MeuSite/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Resumo
+from django.db.models import Q 
+from django.views.generic.base import View
 
 def home(request):
     '''
@@ -6,4 +9,20 @@ def home(request):
     Renders the home.html template.
     '''
     return render(request, 'MeuSite/home.html')
+
+class ResumoListView(View):
+
+    def get(self, request, *args, **kwargs):
+        resumos = Resumo.objects.all().order_by('-data_criacao')
+        contexto = {'pessoas': resumos}        
+        return render(request, 'MeuSite/listaResumos.html', contexto)
     
+def resultadoBusca(request):
+    titulo_busca = request.GET.get('titulo')
+    resumos = Resumo.objects.all()
+
+    if titulo_busca:
+        resumos = resumos.filter(Q(titulo__icontains=titulo_busca))
+        
+    contexto = {'pessoas': resumos}
+    return render(request, 'MeuSite/listaResumos.html', contexto)
