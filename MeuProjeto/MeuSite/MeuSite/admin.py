161e1e47f22ced9c034aff5a8639dab0b657a5af
mvnevares <marinanevares@aluno.puc-rio.br>
2025-10-30 18:24:19 +0000

    WIP: minhas alterações

---
+++ MeuProjeto/MeuSite/MeuSite/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import UsuarioPerfil, Resumo, Comentario, Material
+
+admin.site.register(UsuarioPerfil)
+admin.site.register(Resumo)
+admin.site.register(Comentario)
+admin.site.register(Material)
--- MeuProjeto/MeuSite/MeuSite/urls.py
+++ MeuProjeto/MeuSite/MeuSite/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from MeuSite import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
